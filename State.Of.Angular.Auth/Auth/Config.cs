be2b7761f097e9ca532f235bbc02b0a32216afea
Craig Taylor <taylorc5150@gmail.com>
2018-04-14 22:11:52 +1000

    added angular and identity projects

---
+++ State.Of.Angular.Auth/Auth/Config.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace State.Of.Angular.Auth.Auth
+{
+  public class Config
+  {
+    public static IEnumerable<ApiResource> GetApiResources()
+    {
+      return new List<ApiResource>
+      {
+        new ApiResource("api1", "My API")
+      };
+    }
+
+    public static IEnumerable<Client> GetClients()
+    {
+      return new List<Client>
+      {
+        new Client
+        {
+          ClientId = "client",
+
+          // no interactive user, use the clientid/secret for authentication
+          AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+          // secret for authentication
+          ClientSecrets =
+          {
+            new Secret("secret".Sha256())
+          },
+
+          // scopes that client has access to
+          AllowedScopes = { "api1" }
+        }
+      };
+    }
+
+  }
+}
